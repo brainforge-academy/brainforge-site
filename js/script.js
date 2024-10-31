4c56857c5edb1331e756b45c8b7182404b1a0139
brainforge-learning-solutions <brainforge.in@gmail.com>
2024-10-31 09:18:30 +0530

    Final Simple Cut

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let currentIndex = 0;
-const sections = document.querySelectorAll('.horizontal-scroll .section');
-
-function scrollHorizontally() {
-    currentIndex = (currentIndex + 1) % sections.length;
-    document.querySelector('.horizontal-scroll').style.transform = `translateX(-${currentIndex * 50}%)`;
-}
-
-setInterval(scrollHorizontally, 5000);
